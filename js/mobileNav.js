817b860a76ccde3dd3c49365b604e710eea6f1a8
Shoyo <shoyo@pop-os.localdomain>
2022-10-23 12:12:36 +0530

    first commit

---
+++ js/mobileNav.js
@@ -0,0 +1,7 @@
+const button = document.querySelector(".header__menu");
+const nav = document.querySelector(".header__nav");
+
+button.addEventListener("click",()=>{
+    nav.classList.toggle("show-nav");
+    button.classList.toggle("menu-close");
+});
